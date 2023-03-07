2eb30f7988c8c835d9feb603befffa3bd0e21ba9
Muhammad Mubshar Hussain <75236871+mubsharhussain@users.noreply.github.com>
2023-03-07 21:23:50 +0500

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,5 @@
+#Write a Python program that will accept the base and height of a triangle and compute its area.
+base=int(input("enter the base = "))
+height=int(input("enter the height = "))
+area=(height*base)/2
+print(area)
